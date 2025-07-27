fceba7aa99de1ade93a9bfef445b953658b185c4
Amitbikram Jain <jain0012@e.ntu.edu.sg>
2025-07-27 18:30:18 +0800

    made some changes to main.go

--- main.go
+++ main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("This is my quiz game! Let's start playing!")
+
+	var name string
+	fmt.Printf("Enter your name: ")
+	fmt.Scan(&name)
+	fmt.Printf("Welcome to the quiz game, %v!\n", name)
+
+	fmt.Printf("Enter your age: ")
+	var age int
+	fmt.Scan(&age)
+	if age < 10 {
+		fmt.Println("Sorry, you're too young to play this game. Please come back when you are older.")
+		return
+	} else {
+		fmt.Printf("Great! You are old enough to play the game, %v.\n", name)
+	}
+
 }
